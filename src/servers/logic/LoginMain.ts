5d8f3434a66a9a16a66decb83e602ac394a5781f
啊安 <441829663@qq.com>
2023-10-16 14:10:34 +0800

    Update LoginMain.ts

--- src/servers/logic/LoginMain.ts
+++ src/servers/logic/LoginMain.ts
@@ -12,6 +12,8 @@ export default class LoginMain extends BaseServerLogic {
     public initInstance(): void {
         //监听RPC消息
         TSEventCenter.Instance.bind(RpcEvent.SayHello, this, this.test);
+        //监听后端服消息
+        TSEventCenter.Instance.bind(KarlCatEvent.BackendServerDoFuntion + "101_1", this, this.changeName);
     }
 
 
@@ -20,8 +22,13 @@ export default class LoginMain extends BaseServerLogic {
     }
 
     
-    test(msg: string) {
+    public test(msg: string) {
         console.log("test say ", msg)
+    }
+
+    //以下代码是示例接收后端服的代码 lanlu.IPt101_1_tos会报错，因为我生成的proto文件没有101这个协议
+    public async changeName(msg: lanlu.IPt101_1_tos, session: Session, next: Function) {
+        console.log("changeName ", msg)
     }
 
 }
