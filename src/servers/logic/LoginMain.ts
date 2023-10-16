28c6cd68d1df07082b7b2c40ca643b420fa40484
啊安 <441829663@qq.com>
2023-10-16 14:08:11 +0800

    Update LoginMain.ts

--- src/servers/logic/LoginMain.ts
+++ src/servers/logic/LoginMain.ts
@@ -10,7 +10,7 @@ export default class LoginMain extends BaseServerLogic {
     }
 
     public initInstance(): void {
-        //接收后端服数据 ---注意，后端服只能接收到前端服的消息以及RPC消息
+        //监听RPC消息
         TSEventCenter.Instance.bind(RpcEvent.SayHello, this, this.test);
     }
 
@@ -19,7 +19,7 @@ export default class LoginMain extends BaseServerLogic {
         TSEventCenter.Instance.unbind(RpcEvent.SayHello, this,);
     }
 
-    //romote是用于服务器间通讯的，因此，此处是来自Handler调用的
+    
     test(msg: string) {
         console.log("test say ", msg)
     }
